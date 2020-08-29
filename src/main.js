ef37e2ede09efa5384c2188e78493606d1d35d04
Tuan Tuan LE <tlcong@linagora.com>
2020-08-29 21:40:42 +0700

    Bootstrap project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+// import axios from 'axios'
+// import VueAxios from 'vue-axios'
 import App from './App.vue'
+
+// vue.use(VueAxios, axios)
 
 createApp(App).mount('#app')
